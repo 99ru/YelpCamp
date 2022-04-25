2d4a92403eb853cf8559d186614c396443ad3c85
99ru <yan.99ru@gmail.com>
2022-04-25 19:40:35 +0200

    krillgorv

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const path = require('path');
 const mongoose = require('mongoose'); 
-const Campground = require('./models/campground');
+const Campground = require('./models/campgrounds');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',  {
     useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true,
 });
 
@@ -15,6 +16,7 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 })
 
+
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
   res.render("home");
 });
+
 
 app.get('/makecampground', async (req, res) => {
   const camp = new Campground({ title: 'My backyard', description: 'hobo camping'});
