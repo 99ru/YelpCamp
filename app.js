47c77088edb7f6dcf3dc026b3df0eff72d400aa9
Yan <89448433+99ru@users.noreply.github.com>
2022-04-27 18:09:20 +0200

    can now delete campground

--- app.js
+++ app.js
@@ -59,11 +59,17 @@ app.get('/campgrounds/:id/edit', async (req, res,) => {
   res.render('campgrounds/edit', { campground });
 })
 
-// update the edited campground by id
+// edit campground
 app.put('/campgrounds/:id', async (req, res) => {
   const { id } = req.params; 
   const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}) 
   res.redirect(`/campgrounds/${campground._id}`) 
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params; 
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds');
 })
 
 
