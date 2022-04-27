3bd9d6f0bc80a9616b4bb2ef7884b1b8797cb80b
Yan <89448433+99ru@users.noreply.github.com>
2022-04-27 19:55:40 +0200

    Styling with bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251/1600x900',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error voluptatibus harum reprehenderit ipsa asperiores sit blanditiis aspernatur, quasi rerum nihil.',
+            price
         })
         await camp.save();
     }
